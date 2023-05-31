36641b376b25c5f08c51e64b9b2c2497da44e887
Luferbero <luferberodrigues@gmail.com>
2023-05-31 12:02:24 -0300

    implementando testes junit

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -18,7 +18,7 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+	public static final String SECRET = "50655368566D597133743677397A244326462948404D635166546A576E5A7234";
 
 	private Key getSignKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
